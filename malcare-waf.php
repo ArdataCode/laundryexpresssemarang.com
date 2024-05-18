3e83d0b834f7243381202f7fee3ec4e20f7fe8c9
Ardi <ardata.indonesia@gmail.com>
2024-05-18 14:53:59 +0700

    mmitaw

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/landingu/laundryexpresssemarang.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/landingu/laundryexpresssemarang.com/wp-content/mc_data/');
+	define("MCCONFKEY", 'e54e815fa55ed50894211bbfff924149');
+	include_once('/home/landingu/laundryexpresssemarang.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         's13jBFpUafhz6ff919n5hxHrXa8AELq5hY1CY5HMWegxeJutfuGeTmyFRYAiJio8');
+define('SECURE_AUTH_KEY',  'GNe4WrsjxxDAm2fMWyndjEHrNSHuNTS3CnpJDRX9so9IgBirB0nYMng11PMrgLfn');
+define('LOGGED_IN_KEY',    'tUwJxC9U5CsQPMU3HqhD0mUj1hrord8qEXoWf1fxQb6dgS14pb558n7GxK4aM0z2');
+define('NONCE_KEY',        'ooBgvwUIIHncDzsxSNSMieuU4qRS9XP3ScdpNCEMLwLBjeB0VhwBEjMcnxGiEvNF');
+define('AUTH_SALT',        'YqLsK4ACf4Bac32YMsdKu5w1E1qT7NBA45KgXbGG7zzFiCYGP6bzMf5QzA5UUFwh');
+define('SECURE_AUTH_SALT', 'BFnS3YBHChQr1B5U95s1aRYinvgLVVyQXRhs3u1X31c3LW3bCYQmzsYJtH9Iww6t');
+define('LOGGED_IN_SALT',   'wwF05CxSJujvtRrybd8JuuC7x0jwCEaNzHuva8QpLTz2Qdibg1Tx7Qjd3aWR6stj');
+define('NONCE_SALT',       'RMxYVCeXPTLLxMhIBY6mwx86sf5LvCMDTsoU7PwhXfMy8EjwD603u4oDuMPI4hpb');
